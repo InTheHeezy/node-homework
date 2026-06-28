dd8f91a13b9494240e86d3e78bbc796e14977157
Tonny <tonnyan30@gmail.com>
2026-06-28 15:42:06 -0700

    fs creates sample.txt file

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -3,9 +3,15 @@ const path = require('path');
 
 
 // Write a sample file for demonstration
-
+const filePath = path.join(__dirname, 'sample.txt');
+fs.writeFile(filePath, 'Hello async world!', 'utf8', (err) => {
+  if (err) {
+    console.log("Write file failed:", err.message);
+    return;
+  }
+  console.log('File created successfully');
+})
 // 1. Callback style
-
 
   // Callback hell example (test and leave it in comments):
 
