6fec20feb98bed4cf386a29e1a97d74406d42526
Tonny <tonnyan30@gmail.com>
2026-07-05 16:29:26 -0700

    async/await

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Write a sample file for demonstration
 const filePath = path.join(__dirname, 'sample.txt');
-fs.writeFile(filePath, 'Hello async world!', 'utf8', (err) => {
+fs.writeFile(filePath, 'Hello, async world!', 'utf8', (err) => {
   if (err) {
     console.log("Write file failed:", err.message);
     return;
@@ -18,37 +18,53 @@ fs.readFile(filePath, "utf8", (err, content) => {
     console.log("File read failed:", err.message);
     return;
   }
-  console.log('File content:', content);
+  console.log('Callback style content:', content);
 });
 
-  // Callback hell example (test and leave it in comments):
-  // fs.writeFile(filePath, 'Hello async world!', 'utf8', (err) => {
-  // if (err) {
-  //   console.log("Write file failed:", err.message);
-  //   return;
-  // }
-  // console.log('File created successfully');
+// Callback hell example (test and leave it in comments):
+// fs.writeFile(filePath, 'Hello async world!', 'utf8', (err) => {
+//  if (err) {
+//   console.log("Write file failed:", err.message);
+//   return;
+//  }
+//  console.log('File created successfully');
 
-  // fs.readFile(filePath, "utf8", (err, content) => {
-  //   if (err) {
-  //     console.log("File read failed:", err.message);
-  //     return;
-  //   }
-  //   console.log('File content:', content);
-  //   }); 
-  // }); 
-  // Big difference is read file is inside write file.   
+//  fs.readFile(filePath, "utf8", (err, content) => {
+//    if (err) {
+//      console.log("File read failed:", err.message);
+//      return;
+//    }
+//    console.log('File content:', content);
+//   }); closes read file
+// }); closes write file
+// Big difference is read file is inside write file.   
 
-  // 2. Promise style
-  fs.writeFile(filePath, 'Hello async world!', 'utf8')
-    .then(() => {
-      console.log('File created successfully (Promise)');
-      return fs.readFile(filePath, 'utf8');
-    })
-    .then((content) =>{
-      console.log('FileC content (Promise):', content);
-    })
-    .catch((err) => {
-      console.log('Promise operation failed:', err.message);
-    })
-      // 3. Async/Await style
+// 2. Promise style
+fsProm.writeFile(filePath, 'Hello, async world!', 'utf8')
+  .then(() => {
+    //console.log('File created successfully (Promise)');
+    return fsProm.readFile(filePath, 'utf8');
+  })
+  .then((content) =>{
+    console.log('Promise style content:', content);
+  })
+  .catch((err) => {
+    console.log('Promise operation failed:', err.message);
+  })
+  
+// 3. Async/Await style
+async function run() {
+  try {
+    
+    await fsProm.writeFile(filePath, 'Hello, async world!', 'utf8');
+    //console.log('File created successfully (Async/Await)');
+
+    const content = await fsProm.readFile(filePath, 'utf8');
+    console.log('Async/Await style content:', content);
+
+  } catch (err) {
+    console.log('Async/Await operation failed:', err.message);
+  }
+}
+
+run();
