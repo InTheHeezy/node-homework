ae350243ee06e3a72d475748cea355ea7d45a900
Tonny <tonnyan30@gmail.com>
2026-07-05 16:00:25 -0700

    callback hell example

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -21,6 +21,22 @@ fs.readFile(filePath, "utf8", (err, content) => {
 });
 
   // Callback hell example (test and leave it in comments):
+  // fs.writeFile(filePath, 'Hello async world!', 'utf8', (err) => {
+  // if (err) {
+  //   console.log("Write file failed:", err.message);
+  //   return;
+  // }
+  // console.log('File created successfully');
+
+  // fs.readFile(filePath, "utf8", (err, content) => {
+  //   if (err) {
+  //     console.log("File read failed:", err.message);
+  //     return;
+  //   }
+  //   console.log('File content:', content);
+  //   }); 
+  // }); 
+  // Big difference is read file is inside write file.   
 
 
   // 2. Promise style
