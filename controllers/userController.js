f47b64d9d80793035d6f4c7ae175a5d5f8424b24
Tonny <tonnyan30@gmail.com>
2026-07-18 17:20:00 -0700

    logon function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 global.users = global.users || [];
-global.userid = global.userid || null;
+global.user_id = global.user_id || null;
 
 function register(req, res) {
     try {
@@ -16,11 +16,39 @@ function register(req, res) {
         }
 
         global.users.push(newUser)
-        global.userid = newUser;
+        global.user_id = newUser;
 
         return res.status(201).json({
             message: "User registered successfully",
             user: { name: newUser.name, email: newUser.email }
+        });
+
+    } catch(error) {
+        return res.status(500).json({ error: "Server error" })
+    }
+}
+
+function logon(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ error: "Missing account information"});
+        }
+
+        const validUser = global.users.find(user => 
+            user.email === email && user.password === password
+        );
+
+        if (!validUser) {
+            return res.status(401).json({ error: "Combination of email and password not found"})
+        } 
+
+        global.user_id = validUser;
+            
+        return res.status(200).json({
+            message: "User logged on successfully",
+            user: { name: validUser.name, email: validUser.email }
         });
 
     } catch(error) {
