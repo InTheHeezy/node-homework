12cf44ce6f4b1f4df4bb4c9244c43a98c7a57b31
Tonny <tonnyan30@gmail.com>
2026-07-19 15:54:36 -0700

    add request id middleware

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const dogsRouter = require("./routes/dogs");
+const { randomUUID } = require("crypto");
 
 const app = express();
 
@@ -11,6 +12,12 @@ app.use(express.json());
 app.use("/", dogsRouter);// Do not remove this line
 
 app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+app.use((req, res, next) => {
+  req.requestId = randomUUID();
+  res.setHeader("X-Request-Id", req.requestId);
+  next();
+})
 
 if (require.main === module) {
   app.listen(3000, () => {
