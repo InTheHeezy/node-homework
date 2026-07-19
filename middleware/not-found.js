b12fec563c321420753ac42d7fb28350b6ee6446
Tonny <tonnyan30@gmail.com>
2026-07-18 18:19:48 -0700

    spacing

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,11 +1,9 @@
 module.exports = function(req, res, next) {
     try {
-
         return res.status(404).json({
             error: "Not found",
             message: `Could not find the path for ${req.path}`
         })
-
     } catch (error) {
         return res.status(500).json({ error: "Server error"});
     }
