feffc49c442f526f965ef099e430b93489af2231
Tonny <tonnyan30@gmail.com>
2026-07-18 18:17:40 -0700

    removed comments

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,16 +3,6 @@ const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-
-/* @TODO 
-Full routes for app.js
-
-POST /api/users/register
-POST /api/users/logon
-POST /api/users/logoff
-
-*/
-
 route.post("/register", userController.register);
 route.post("/logon", userController.logon);
 route.post("logoff", userController.logoff);
