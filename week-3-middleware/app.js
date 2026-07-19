413ed679e37a7f6427f192cd2d03b601d3cfda03
Tonny <tonnyan30@gmail.com>
2026-07-18 18:38:49 -0700

    added express

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const path = require("path");
 const dogsRouter = require("./routes/dogs");
 
 const app = express();
 
 // Assignment 3b and 3c ask you to add middleware in this file.
 
-
-
+app.use(express.json());
 
 app.use("/", dogsRouter);// Do not remove this line
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 if (require.main === module) {
   app.listen(3000, () => {
