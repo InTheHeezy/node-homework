654bec0ae43fe7cf2b340f473b9724db41b6d581
Tonny <tonnyan30@gmail.com>
2026-07-19 17:05:04 -0700

    removed error handler part

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,10 +1,6 @@
 module.exports = function(req, res, next) {
-    try {
-        return res.status(404).json({
-            error: "Not found",
-            message: `Could not find the path for ${req.path}`
-        })
-    } catch (error) {
-        return res.status(500).json({ error: "Server error"});
-    }
+    return res.status(404).json({
+        error: "Not found",
+            essage: `Could not find the path for ${req.path}`
+    })
 };
