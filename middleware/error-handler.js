61056d40c985e75d069cb4044aa90f9716496c6c
Tonny <tonnyan30@gmail.com>
2026-07-19 17:27:05 -0700

    fixed error messages

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,5 @@
 module.exports = function(err, req, res, next) {
    return res.status(500).json({
-    error: "Server Error",
-    message: err.message || "The server has encountered an unexpected error"
+      message: "Unexpected server error"
    });
 }
--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,6 +1,5 @@
 module.exports = function(req, res, next) {
     return res.status(404).json({
-        error: "Not found",
-            essage: `Could not find the path for ${req.path}`
+        message: "Not found"
     })
 };
