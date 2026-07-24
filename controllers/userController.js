7e32136b6679f9836a20f0842c153006f15971d2
Tonny <tonnyan30@gmail.com>
2026-07-24 16:26:59 -0700

    removed try blocks and changed send status

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,68 +1,46 @@
 function register(req, res) {
-    try {
-        const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
 
-        if (!name || !email || !password) {
-            return res.status(400).json({ error: "Missing account information"});
-        }
+    const id = Date.now().toString();
 
-        const id = Date.now().toString();
+    const newUser = {
+        id,
+        name,
+        email,
+        password
+    };
 
-        const newUser = {
-            id,
-            name,
-            email,
-            password
-        }
+    global.users.push(newUser);
+    global.user_id = newUser;
 
-        global.users.push(newUser)
-        global.user_id = newUser;
-
-        return res.status(201).json({
-            name: newUser.name, 
-            email: newUser.email 
-        });
-
-    } catch(error) {
-        return res.status(500).json({ error: "Server error" })
-    }
+    return res.status(201).json({
+        name: newUser.name, 
+        email: newUser.email 
+    });
 }
 
 function logon(req, res) {
-    try {
-        const { email, password } = req.body;
+    const { email, password } = req.body;
 
-        if (!email || !password) {
-            return res.status(400).json({ error: "Missing account information"});
-        }
+    const validUser = global.users.find(user => 
+        user.email === email && user.password === password
+    );
 
-        const validUser = global.users.find(user => 
-            user.email === email && user.password === password
-        );
+    if (!validUser) {
+        return res.status(401).json({ error: "Invalid email or password"});
+    } 
 
-        if (!validUser) {
-            return res.status(401).json({ error: "Invalid email or password"})
-        } 
-
-        global.user_id = validUser;
-        
-        return res.status(200).json({
-            name: validUser.name, 
-            email: validUser.email 
-        });
-
-    } catch(error) {
-        return res.status(500).json({ error: "Server error" })
-    }
+    global.user_id = validUser;
+    
+    return res.status(200).json({
+        name: validUser.name, 
+        email: validUser.email 
+    });
 }
 
 function logoff(req, res) {
-    try {
-        global.user_id = null;
-        return res.status(200).json({ message: "Logged off successfully"});
-    } catch(error) {
-        return res.status(500).json({ error: "Server error" })
-    }
+    global.user_id = null;
+    return res.sendStatus(200);
 }
 
 module.exports = {
