2d9ba12c80d6e9f3cb7eb20e818bb12a3074ab81
Tonny <tonnyan30@gmail.com>
2026-07-25 16:36:39 -0700

    added task counter

---
+++ controllers/taskController.js
@@ -0,0 +1,11 @@
+const taskCounter = (() => {
+  let lastTaskNumber = 0;
+  return () => {
+    lastTaskNumber += 1;
+    return lastTaskNumber;
+  };
+})();
+
+module.exports = {
+    taskCounter
+}
