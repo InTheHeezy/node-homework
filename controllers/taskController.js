69fa84430d1bf2759c3a99bbc19de9e11b6e7cf6
Tonny <tonnyan30@gmail.com>
2026-07-25 17:36:59 -0700

    added create function

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,3 +1,5 @@
+const { taskSchema, patchTaskSchema } = require("../validation/taskSchema");
+
 const taskCounter = (() => {
   let lastTaskNumber = 0;
   return () => {
@@ -6,6 +8,25 @@ const taskCounter = (() => {
   };
 })();
 
+async function create(req, res) {
+  if (!req.body) req.body = {};
+  const { error, value } = taskSchema.validate(req.body, { abortEarly: false });
+  if (error) return res.status(400).json({ message: error.message });
+
+  const newTask = {
+    id: taskCounter(),
+    userId: global.user_id.email,
+    ...value
+  };
+
+  global.tasks.push(newTask);
+
+  const { userId, ...sanitizedTask } = newTask;
+
+  return res.status(201).json(sanitizedTask);
+}
+
 module.exports = {
+    create,
     taskCounter
 }
