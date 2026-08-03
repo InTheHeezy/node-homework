e18cea3408f219f8db76ecbd455d9a1a39d565ee
Tonny <tonnyan30@gmail.com>
2026-08-02 17:30:39 -0700

    2b added graceful shutdown

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const notFound = require("./middleware/not-found");
 const errorHandler = require("./middleware/error-handler");
 const authMiddleware = require("./middleware/auth");
 const taskRouter = require("./routes/taskRoutes");
+const pool = require("./db/pg-pool");
 
 const app = express();
 
@@ -24,5 +25,22 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
 });
+
+const handleShutdown = async () => {
+  console.log("Shutting down server...");
+  server.close(async () => {
+    try {
+      await pool.end();
+      console.log("Database pool has ended.");
+      process.exit(0);
+    } catch (err) {
+      console.error("Error closing database pool:", err);
+      process.exit(1);
+    }
+  });
+};
+
+process.on("SIGINT", handleShutdown);
+//process.on("SIGTERM", handleShutdown); this is for cloud platforms
 
 module.exports = { app, server };
