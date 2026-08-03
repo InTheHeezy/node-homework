c1bc2dc1841a2cf8a2482e7362a014a0c045951a
Tonny <tonnyan30@gmail.com>
2026-08-03 12:14:45 -0700

    fixed health check

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,13 @@
 const errorHandler = (err, req, res, next) => {
    if (err.code === "ECONNREFUSED" && err.port === 5432) { // the postgresql port
     console.log("The database connection was refused.  Is your database service running?");
-   } else {
-      console.error(err);
-   }
+
+    return res.status(500).json({
+         error: "Database connection refused"
+      });
+   } 
+   
+   console.error(err);
    
    return res.status(500).json({
       error: "Internal Server Error"
