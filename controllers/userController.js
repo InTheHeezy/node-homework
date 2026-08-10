18c6b32daa8af0171b3488b34d6b60a44b87177d
Tonny <tonnyan30@gmail.com>
2026-08-09 17:46:11 -0700

    added missing req data check

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,6 +50,11 @@ async function register(req, res, next) {
 }
 
 async function logon(req, res) {
+
+    if (!req.body || !req.body.email || !req.body.password) {
+        return res.status(400).json({ message: "Email and password are required." });
+    }
+
     const { email, password } = req.body;
 
     const cleanEmail = email.toLowerCase();
