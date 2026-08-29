f2a01f0ae535434c678e9a0a124e67caf49f1ea7
Tonny <tonnyan30@gmail.com>
2026-08-28 17:55:02 -0700

    added test case 14

--- test/taskController.test.js
+++ test/taskController.test.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 process.env.DATABASE_URL = process.env.TEST_DATABASE_URL; // point to the test database!
 const prisma = require("../db/prisma");
 const httpMocks = require("node-mocks-http");
+const { EventEmitter } = require('events');
+const { waitForRouteHandlerCompletion } = require("./waitForRouteHandlerCompletion.js")
+
 const {
   index,
   show,
@@ -31,3 +34,17 @@ afterAll(() => {
   prisma.$disconnect();
 })
 
+describe("testing task creation", () => {
+   it("14. cant create a task without a user id", async () => {
+    const req = httpMocks.createRequest({
+        method: "POST",
+        body: { title: "first task" },
+    });
+    saveRes = httpMocks.createResponse({eventEmitter: EventEmitter});
+    try {
+      await waitForRouteHandlerCompletion(create,req, saveRes);
+    } catch (e) {
+        expect(e.name).toBe("TypeError");
+    }
+  });
+})
