eb4f3c826bcaaf402f65ee6fd5bdd74fc4f65a6a
Tonny <tonnyan30@gmail.com>
2026-08-29 20:07:10 -0700

    added test case 46

---
+++ test/user.function.test.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const request = require("supertest");
+process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
+const prisma = require("../db/prisma");
+let agent;
+let saveRes;
+const { app, server } = require("../app");
+
+beforeAll(async () => {
+  // clear database
+  await prisma.Task.deleteMany(); // delete all tasks
+  await prisma.User.deleteMany(); // delete all users 
+  agent = request.agent(app);
+});
+
+afterAll(async () => {
+  prisma.$disconnect();
+  server.close();
+});
+
+describe("register a user ", () => {
+  let saveRes = null; // we'll declare this out here, so that we can reference it in several tests
+  it("46. it creates the user entry", async () => {
+    const newUser = {
+      name: "John Deere",
+      email: "jdeere@example.com",
+      password: "Pa$$word20",
+    };
+    saveRes = await agent.post("/api/users/register").send(newUser);
+    expect(saveRes.status).toBe(201);
+  });
+})
