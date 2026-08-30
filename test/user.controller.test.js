c0694e42e4c1382ff54f1d6174c8f52e1dcd2e43
Tonny <tonnyan30@gmail.com>
2026-08-29 17:06:52 -0700

    added test case 33

---
+++ test/user.controller.test.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
+const waitForRouteHandlerCompletion = require("./waitForRouteHandlerCompletion");
+const prisma = require("../db/prisma");
+const httpMocks = require("node-mocks-http");
+const { register, logoff, logon } = require("../controllers/userController");
+const jwtMiddleware = require("../middleware/jwtMiddleware");
+const jwt = require("jsonwebtoken");
+
+// a few useful globals
+let saveRes = null;
+let saveData = null;
+
+const cookie = require("cookie");
+function MockResponseWithCookies() {
+  const res = httpMocks.createResponse({
+    eventEmitter: EventEmitter,
+  });
+  res.cookie = (name, value, options = {}) => {
+    const serialized = cookie.serialize(name, String(value), options);
+    let currentHeader = res.getHeader("Set-Cookie");
+    if (currentHeader === undefined) {
+      currentHeader = [];
+    }
+    currentHeader.push(serialized);
+    res.setHeader("Set-Cookie", currentHeader);
+  };
+  return res;
+}
+
+beforeAll(async () => {
+  // clear database
+  await prisma.Task.deleteMany(); // delete all tasks
+  await prisma.User.deleteMany(); // delete all users
+});
+
+afterAll(() => {
+  prisma.$disconnect();
+});
+
+let jwtCookie;
+
+describe("testing logon, register, and logoff", () => {
+  it("33. A user can be registered.", async () => {
+    const req = httpMocks.createRequest({
+      method: "POST",
+      body: { name: "Bob", email: "bob@sample.com", password: "Pa$$word20" },
+    });
+    saveRes = MockResponseWithCookies();
+    await waitForRouteHandlerCompletion(register, req, saveRes);
+    expect(saveRes.statusCode).toBe(201); // success!
+  });
+});
