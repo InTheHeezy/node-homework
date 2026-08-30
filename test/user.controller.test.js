b840c1f510dafa8c2b96da16881eae3e416391c1
Tonny <tonnyan30@gmail.com>
2026-08-29 18:13:41 -0700

    added test case 36

--- test/user.controller.test.js
+++ test/user.controller.test.js
@@ -62,16 +62,25 @@ describe("testing logon, register, and logoff", () => {
     expect(saveRes.statusCode).toBe(200); // success!
 
     const setCookieArray = saveRes.get("Set-Cookie");
+    //console.log("set cookie array?:", Array.isArray(setCookieArray));
+    //console.log("Inside set cookie:", setCookieArray);
     if (setCookieArray && setCookieArray.length > 0) {
-      jwtCookie = setCookieArray;
+      jwtCookie = String(setCookieArray);
     }
-
+    //console.log("big jwtCookie string:", jwtCookie);
   });
 
   it("35. A string in the cookie array starts with \"jwt=\"", () => {
     expect(jwtCookie).toBeDefined();
-    expect(jwtCookie.length).toBeGreaterThan(0);
-    const hasJwtCookie = jwtCookie.some(cookieStr => cookieStr.startsWith("jwt="));
-    expect(hasJwtCookie).toBe(true);
+    expect(jwtCookie.startsWith("jwt=")).toBe(true);
   });
+
+  it("36. That string contains \"HttpOnly;\"", () => {
+    expect(jwtCookie).toBeDefined();
+    expect(jwtCookie).toContain("HttpOnly;");
+  });
+
+   it("37. The returned data from the register has the expected name.", () => {
+    
+   });
 });
