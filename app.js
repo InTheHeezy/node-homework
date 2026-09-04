81ebd4d0004374c47ba194b811b9ee0b9f2d0c38
Tonny <tonnyan30@gmail.com>
2026-09-04 16:11:33 -0700

    added reCAPTCHA token stuff

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(
     max: 100, // limit each IP to 100 requests per windowMs
   }),
 );
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 app.use(xss());
 
