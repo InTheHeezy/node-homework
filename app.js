8fe522e6d886c407377ec22dd57c35ce579ead09
Tonny <tonnyan30@gmail.com>
2026-09-12 16:51:48 -0700

    added google logon public route

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.get("/health", async (req, res) => {
   }
 });
 
+app.post("/users/googleLogon", userRouter); 
 app.use("/api/users", jwtMiddleware, userRouter);
 app.use("/api/tasks", jwtMiddleware, taskRouter);
 app.use("/api/analytics", jwtMiddleware, analyticsRouter);
